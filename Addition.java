e2bfd5dcb8715092ec8d813952dffe0701971ab2
Chandanarkxworkz <chandanark.xworkz@gmail.com>
2023-08-22 12:31:38 +0530

    using methods

---
+++ Addition.java
@@ -0,0 +1,33 @@
+class Addition{
+	public static void main(String Addition[]){
+	 getAdd (20,30);
+	 getAdd (10,50);
+	 getAdd (80,20);
+	 getAdd (60,20);
+	 getAdd (100,20);
+	 getAdd (60,200);
+	 getAdd (6000,20);
+	 getAdd (5000,2000);
+	 getAdd (800,500);
+	 getAdd (400,300);
+	 getAdd (600,20);
+	 getAdd (450,250);
+	 getAdd (700,200);
+	 getAdd (1000,2000);
+	 getAdd (60,80);
+	 getAdd (60,60);
+	 getAdd (70,50);
+	 getAdd (60,80);
+	 getAdd (90,20);
+	 getAdd (90,200);
+}
+	 public static void getAdd(int a,int b){
+		 
+		 System.out.println("get Add started");
+	 
+	 System.out.println(" Addition of two number is : ");
+	 System.out.println(a+b);
+	 
+	 System.out.println("get Add ended");
+ }
+}
---
+++ Multiplication.java
@@ -0,0 +1,32 @@
+class Multiplication{
+	public static void main(String Multiple[]){
+	 getMultiple(20,30);
+	 getMultiple (10,50);
+	 getMultiple (80,20);
+	 getMultiple (60,20);
+	 getMultiple (100,20);
+	 getMultiple (60,200);
+	 getMultiple (6000,250);
+	 getMultiple (5000,2000);
+	 getMultiple (800,500);
+	 getMultiple (400,300);
+	 getMultiple (600,20);
+	 getMultiple (450,250);
+	 getMultiple (700,200);
+	 getMultiple (1000,2000);
+	 getMultiple (60,80);
+	 getMultiple (60,60);
+	 getMultiple (70,50);
+	 getMultiple (60,80);
+	 getMultiple (90,20);
+	 getMultiple (90,300);
+}
+	 public static void getMultiple(int a,int b){
+	 System.out.println("get Multiple started");
+	 
+	 System.out.println("multiplication  of two number is : ");
+	 System.out.println(a*b);
+	 
+	 System.out.println("get Multiple ended");
+ }
+}
